26870d3cdbd9b5ddbe33b0a93b66b67eb51270ab
L Lee <lauralee.sh@gmail.com>
2018-10-02 02:02:49 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import PandaCard from "./components/PandaCard";
 import Title from "./components/Title";
 import Score from "./components/Score";
+//refernce a json file of all pandas cards info
 import pandas from "./pandas.json";
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
     selectedID: []
   };
 
+  //function to render different images to the screen in a random order. 
   shuffleCards = () => {
     for (let i =0; i<pandas.length; i++) {
       let elem = pandas[i];
@@ -21,9 +23,12 @@ class App extends React.Component {
     }
   }
 
+  //Each image listens for click events.
   onClickfun = (id) => {
     console.log(id);
     console.log(this.state.selectedID);
+    //keep track of the user's selected images and score
+    //increment score if continue clicking different images
     if (!this.state.selectedID.includes(id)){
       this.setState({
         selectedID: this.state.selectedID.concat(id),
@@ -31,6 +36,7 @@ class App extends React.Component {
         })
         console.log(this.state.userScore);
     }
+    //user's score reset to 0 if they click the same image more than once.
     else {
       this.setState({
         selectedID: [],
@@ -41,6 +47,7 @@ class App extends React.Component {
     this.shuffleCards();
   }
 
+  //for rendering cards on screen (to be called in render)
   createCards = () => {
     let cards = []
     for (let i = 0; i < 12; i++) {
@@ -55,7 +62,7 @@ class App extends React.Component {
     return cards
   }
   
-
+  //keep track of the top score
   setTopScore = () => {
     if (this.state.userScore > this.state.topScore) {
       return this.setState({topScore: this.state.userScore})
@@ -66,7 +73,8 @@ class App extends React.Component {
 
     return (
       <Wrapper>
-        <Title>Pandas</Title>
+        <Title>Pandas Clicky Game!</Title>
+        <Score>Click on an image to earn points, but don't click on any more than once!</Score>
         <Score>Score: {this.state.userScore}</Score>
         <Score>Top Score: {this.state.topScore}</Score>
         {this.createCards()}
--- src/components/PandaCard/PandaCard.js
+++ src/components/PandaCard/PandaCard.js
@@ -5,6 +5,7 @@ const PandaCard = props => (
   <div className="card">
     <div className="img-container">
       <img alt={props.name} src={props.image} 
+      //each image listen to onClick event and pass id
       onClick={() => props.onClickfun(props.id)}
       />
     </div>
